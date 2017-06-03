d7bd2849dab2d0aeef8530dcc21e1f1be8a9e24d
Eugénie <eugenielm@users.noreply.github.com>
2017-06-02 19:21:45 -0700

    WIP deployment - remove trailing / from the index URL

--- grocerystore/urls.py
+++ grocerystore/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'grocerystore'
 urlpatterns = [
     # grocerystore/
-    url(r'^/$', views.IndexView.as_view(), name='index'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^register/$', views.UserRegisterView.as_view(), name='register'),
     url(r'^login/$', views.UserLoginView.as_view(), name='login'),
     url(r'^logout/$', views.log_out, name='logout'),
